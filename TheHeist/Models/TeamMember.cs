edcc3266a7c9b36c5a768e63579afdd62000a5b0
Brian Neal <brianbneal@gmail.com>
2022-01-18 20:09:08 -0600

    Add project files.

---
+++ TheHeist/Models/TeamMember.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHeist.Models
+{
+    internal class TeamMember
+    {
+        public TeamMember(string name, int skill, decimal courage)
+        {
+            Name = name;
+            SkillLevel = skill;
+            CourageFactor = courage;
+        }
+
+        //A team member will have a name, a skill Level and a courage factor.
+        //The skill Level will be a positive integer and the courage factor will be a decimal between 0.0 and 2.0.
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public decimal CourageFactor { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($@"
+Name: {Name}
+Skill Level: {SkillLevel}
+Courage: {CourageFactor}
+");
+        }
+    }
+}
---
+++ TheHeist/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TheHeist.Models;
+
+var title = $@"
+ _______   __         ______   __    __        __      __   ______   __    __  _______         __    __  ________  ______   ______  ________ 
+|       \ |  \       /      \ |  \  |  \      |  \    /  \ /      \ |  \  |  \|       \       |  \  |  \|        \|      \ /      \|        \
+| $$$$$$$\| $$      |  $$$$$$\| $$\ | $$       \$$\  /  $$|  $$$$$$\| $$  | $$| $$$$$$$\      | $$  | $$| $$$$$$$$ \$$$$$$|  $$$$$$\\$$$$$$$$
+| $$__/ $$| $$      | $$__| $$| $$$\| $$        \$$\/  $$ | $$  | $$| $$  | $$| $$__| $$      | $$__| $$| $$__      | $$  | $$___\$$  | $$   
+| $$    $$| $$      | $$    $$| $$$$\ $$         \$$  $$  | $$  | $$| $$  | $$| $$    $$      | $$    $$| $$  \     | $$   \$$    \   | $$   
+| $$$$$$$ | $$      | $$$$$$$$| $$\$$ $$          \$$$$   | $$  | $$| $$  | $$| $$$$$$$\      | $$$$$$$$| $$$$$     | $$   _\$$$$$$\  | $$   
+| $$      | $$_____ | $$  | $$| $$ \$$$$          | $$    | $$__/ $$| $$__/ $$| $$  | $$      | $$  | $$| $$_____  _| $$_ |  \__| $$  | $$   
+| $$      | $$     \| $$  | $$| $$  \$$$          | $$     \$$    $$ \$$    $$| $$  | $$      | $$  | $$| $$     \|   $$ \ \$$    $$  | $$   
+ \$$       \$$$$$$$$ \$$   \$$ \$$   \$$           \$$      \$$$$$$   \$$$$$$  \$$   \$$       \$$   \$$ \$$$$$$$$ \$$$$$$  \$$$$$$    \$$   
+                                                                                                                                             
+                                                                                                                                             
+                                                                                                                                             ";
+var question1 = "Enter team member name:";
+
+Console.WriteLine(title);
+
+Console.WriteLine(question1);
+var name = Console.ReadLine();
+
+bool successful = false;
+int parsedSkill = 0;
+decimal parsedCourage = 0;
+
+while (!successful)
+{
+    Console.WriteLine($"Enter {name}'s skill level:");
+    var skill = Console.ReadLine(); //"1"  1
+    successful = int.TryParse(skill, out parsedSkill);
+
+}
+
+successful = false;
+while (!successful)
+{
+    Console.WriteLine($"Enter {name}'s courage factor:");
+    var courage = Console.ReadLine();
+    successful = decimal.TryParse(courage, out parsedCourage);
+
+}
+
+var teamMember = new TeamMember(name, parsedSkill, parsedCourage);
+teamMember.Print();
